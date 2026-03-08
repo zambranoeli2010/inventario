2903dec763d176f1a40ba77aa351633dc1e15f02
zambranoeli2010 <zambranoeli2010@gmail.com>
2026-03-08 00:19:18 -0400

    uso de bcryptjs para despliegue en render

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import mysql from 'mysql2/promise';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const {
   DB_HOST,
